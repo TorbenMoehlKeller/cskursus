31097f9c77a10f4d88528745020b314b403da192
TorbenMoehlKeller <tkeller@youmail.dk>
2018-11-24 12:53:42 +0100

    New Flow project

---
+++ OPG_Flow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPG_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
