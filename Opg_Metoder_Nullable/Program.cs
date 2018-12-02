5e1dfb1223c0919b3c27df3f7de550d345049acb
TorbenMoehlKeller <tkeller@youmail.dk>
2018-12-02 19:12:03 +0100

    nullable exercise

---
+++ Opg_Metoder_Nullable/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Opg_Metoder_Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool? res;
+            res = HentFraDb(1);
+            Console.WriteLine($"resultat: {res}");
+
+            res = HentFraDb(2);
+            Console.WriteLine($"resultat: {res}");
+
+            res = HentFraDb(3);
+            Console.WriteLine($"resultat: {res}");
+
+
+
+            // Break ud i kode er også muligt
+            // System.Diagnostics.Debugger.Break();
+
+            // Hold Console åbn (ved debug)
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . .");
+                Console.ReadKey();
+            }
+        }
+        static bool? HentFraDb (int tal)
+        {
+            if (tal == 1)
+                return true;
+            if (tal == 2)
+                return false;
+            return null;
+
+        }
+    }
+}
